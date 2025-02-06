993000bbbe4e77c31b33ad4d98fbae5e1d0a9c4a
Kvothe42 <almogn5@gmail.com>
2025-02-06 14:11:43 +0200

    added logging funcuallity and Tape A807 SUpport

--- Tape_Remote.py
+++ Tape_Remote.py
@@ -1,6 +1,32 @@
 import eel
 import serial_console as sercon
 
+# for reading existsing classes in the module sercon (one above)
+import inspect
+
+# for Logging 
+import logging
+import os
+# for parsing Arguments (for logging)
+from app_logger import Logger_c
+
+#  creating local logger
+logger = logging.getLogger(__name__)
+
+#  getting path for folder
+path = os.getcwd()
+# Creating and renaming the methoods from object
+log_c = Logger_c(logger, path)
+print_info = log_c.print_info
+Logging_Decor = log_c.Logging_Decor
+
+# Creating the Argument Parser for File
+args = log_c.Create_Parser()
+
+# wrapping the methood with calling methood intead of pi methood
+eel.expose(log_c.Front_Log)
+
+
 
 
 # Set web files folder
@@ -10,27 +36,14 @@ eel.init('web')
 # Set web files folder and optionally specify which file types to check for eel.expose()
 #   *Default allowed_extensions are: ['.js', '.html', '.txt', '.htm', '.xhtml']eel.init('web', allowed_extensions=['.js', '.html'])@eel.expose  #This is how you expose the py functions below for javascript.
 
-@eel.expose
-def Knob_Perc_SendBack(data):
-    knob_input = str(data)
-    # pformat the sowing strings
-    k_percent, k_id = knob_input.split("|")
-    out_str = f"knob: {k_id} | percent {k_percent}"
-    print(out_str)
-    global knobs_perc
-    knobs_perc[k_id] = k_percent
-    # return str(data)
-
-
 
 
 # Vals Dicts Declaration
-knobs_perc = {"kc1": "", "kc2": "", "kl1": "", "kl2": ""}
-print(knobs_perc)
 conn = "" # conn variable exists globally from the function connecting_serial
 
 
 # initiating serial connection to tape
+@Logging_Decor
 @eel.expose
 def connecting_serial(port = "auto"):
     r"""connecting and listing serial ports 
@@ -50,42 +63,64 @@ def connecting_serial(port = "auto"):
     if port == "auto":
         ports = sercon.Serial_connect()
         if type(ports) == list:
-            print("there is more than 1 port got ports list")
+            print_info("there is more than 1 port got ports list")
             return ports
         elif ports == "there is no serial port connectetd":
             return "there is no serial port connectetd"
-    # can be deleted stays for now only for roll back perpuse
-        # else:
-        #     conn = ports
-        #     if conn.is_open == True:
-        #         # moving to the main page
-        #         eel.go_to_page("tape_interface3.html")()
-        #         return str(conn)
-        
+
+
     else:
         try:
             conn = sercon.Serial_connect(port= port)
             if conn.is_open == True:
-                # moving to the main page
-                eel.go_to_page("tape_interface3.html")()
+                
+                # # moving to the main page
+                # eel.go_to_page("tape_interface3.html")()
+
                 return str(conn)
-        except FileNotFoundError:
+        except FileNotFoundError as ext:
+
+            logger.error(f'com port is not connected anymore please refrase page using f5', exc_info=True)
             return "com port is not connected anymore please refrase page using f5"
 
 
-# initiating a Tape object 
+# Getting the list of supported machines(reading the existing calsses to machines existed)
+@Logging_Decor
 @eel.expose
-def Create_Tape_object():
-    print(conn.is_open)
+def Get_Supported_Tapes():
+    Cls_List = []
+    #  getting the calss list from the module (using fildering with the isClass methood)
+    Raw_Cls_List = inspect.getmembers(sercon, inspect.isclass)
+    for Cls in Raw_Cls_List:
+        Cls = Cls[0]
+        if "Tape_Recorder_" in Cls:
+            Cls = Cls.replace("_", " ")
+            Cls_List.append(Cls)
+    
+    return Cls_List
+
+
+
+
+
+# initiating a Tape object 
+@Logging_Decor
+@eel.expose
+def Create_Tape_object(Mechine):
+    print_info(conn.is_open)
     # Creating Tape Object for commands mathoods
     if conn.is_open == True:
         global Tape
-        Tape = sercon.Tape_Recorder(conn)
-        print("connecting...")
-        print("connected")
-        print(Tape)
+        if Mechine == "A810":
+            Tape = sercon.Tape_Recorder_A810(conn)
+        elif Mechine == "A807":
+            Tape = sercon.Tape_Recorder_A807(conn)   
+        print_info("connecting...")
+        print_info(f"connected - Machine {Mechine}")
+        print_info(Tape)
         return Tape
 
+@Logging_Decor
 @eel.expose
 def Tape_Command(command = "list_commands", command_args = "None"):
     Tape_Command_Dict = {"Play": "Plays Tape ",
@@ -114,20 +149,20 @@ def Tape_Command(command = "list_commands", command_args = "None"):
         else:
             com = f"Tape.{command}({command_args})"
         
-        print(com)
+        print_info(com)
         Command_Response = eval(com)
-        print(Command_Response)
+        print_info(Command_Response)
         return Command_Response
     else: 
         return Tape_Command_Dict
 
+@Logging_Decor
 @eel.expose
 def Timer_Return():
     return Tape.Generic_command("TM?")
 
-# declaring global conn varible for future refrance
-# global conn 
 
+# Checking Number Of ports for opening the correct page 
 ports = sercon.Serial_connect()
 if type(ports) == list:
     eel.start("tape_interface-serial-conn.html", size = (874, 970))             # Eel command for starting app. size is for window sizing    
